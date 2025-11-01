e43e013de4cdb4bb8ed0621105b700ec0e339265
JohnGallemore <122250845+JohnGallemore@users.noreply.github.com>
2025-11-01 18:14:26 -0500

    Changed request in database to opportunity

--- app/models.py
+++ app/models.py
@@ -53,24 +53,24 @@ class Org(db.Model):
 	org_name = db.Column(db.String(255), nullable=False)
 	creator_id = db.Column(db.BigInteger, db.ForeignKey('user.user_id', name='fk_org_creator_id_user_user_id'))
 
-	requests = db.relationship('Request', back_populates='org', lazy='dynamic')
+	opportunitys = db.relationship('opportunity', back_populates='org', lazy='dynamic')
 
 	def __repr__(self):
 		return f"<Org {self.org_id}>"
 
 
-class Request(db.Model):
-	__tablename__ = 'request'
-	request_id = db.Column(db.BigInteger, primary_key=True)
-	org_id = db.Column(db.BigInteger, db.ForeignKey('org.org_id', name='fk_request_org_id_org_org_id'))
+class Opportunity(db.Model):
+	__tablename__ = 'opportunity'
+	opportunity_id = db.Column(db.BigInteger, primary_key=True)
+	org_id = db.Column(db.BigInteger, db.ForeignKey('org.org_id', name='fk_opportunity_org_id_org_org_id'))
 	title = db.Column(db.String(255), nullable=True)
 	description = db.Column(db.Text, nullable=True)
 
-	org = db.relationship('Org', back_populates='requests')
-	pledges = db.relationship('Pledge', back_populates='request', lazy='dynamic')
+	org = db.relationship('Org', back_populates='opportunitys')
+	pledges = db.relationship('Pledge', back_populates='opportunity', lazy='dynamic')
 
 	def __repr__(self):
-		return f"<Request {self.request_id} org={self.org_id}>"
+		return f"<opportunity {self.opportunity_id} org={self.org_id}>"
 
 
 class Scrape(db.Model):
@@ -90,7 +90,7 @@ class Promotes(db.Model):
 	promotes_id = db.Column(db.BigInteger, primary_key=True)
 	group_id = db.Column(db.BigInteger, db.ForeignKey('group.group_id', name='fk_promotes_group_id_group_group_id'))
 	scrape_id = db.Column(db.BigInteger, db.ForeignKey('scrape.scrape_id', name='fk_promotes_scrape_id_scrape_scrape_id'), nullable=True)
-	request_id = db.Column(db.BigInteger, db.ForeignKey('request.request_id', name='fk_promotes_request_id_request_request_id'), nullable=True)
+	opportunity_id = db.Column(db.BigInteger, db.ForeignKey('opportunity.opportunity_id', name='fk_promotes_opportunity_id_opportunity_opportunity_id'), nullable=True)
 	type = db.Column(db.String(50), nullable=True)
 
 	def __repr__(self):
@@ -100,13 +100,13 @@ class Promotes(db.Model):
 class Pledge(db.Model):
 	__tablename__ = 'pledge'
 	pledge_id = db.Column(db.BigInteger, primary_key=True)
-	request_id = db.Column(db.BigInteger, db.ForeignKey('request.request_id', name='fk_pledge_request_id_request_request_id'), nullable=True)
+	opportunity_id = db.Column(db.BigInteger, db.ForeignKey('opportunity.opportunity_id', name='fk_pledge_opportunity_id_opportunity_opportunity_id'), nullable=True)
 	scrape_id = db.Column(db.BigInteger, db.ForeignKey('scrape.scrape_id', name='fk_pledge_scrape_id_scrape_scrape_id'), nullable=True)
 	user_id = db.Column(db.BigInteger, db.ForeignKey('user.user_id', name='fk_pledge_user_id_user_user_id'))
 	type = db.Column(db.String(50), nullable=True)
 
 	user = db.relationship('User', back_populates='pledges')
-	request = db.relationship('Request', back_populates='pledges')
+	opportunity = db.relationship('opportunity', back_populates='pledges')
 	scrape = db.relationship('Scrape', back_populates='pledges')
 
 	def __repr__(self):
--- app/opportunity.py
+++ app/opportunity.py
@@ -2,21 +2,21 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import Request
 from . import db
 
-request_bp = Blueprint('request', __name__)
+opportunity_bp = Blueprint('request', __name__)
 
-@request_bp.route('/request')
+@opportunity_bp.route('/request')
 def request_page():
     return render_template('request.html')
 
-@request_bp.route('/request', methods=['POST'])
-def request_volunteers():
+@opportunity_bp.route('/request', methods=['POST'])
+def create_opportunity():
     title = request.form.get('title')
     org_id = request.form.get('org_id')
     description = request.form.get('description')
 
-    new_request = Request(title=title, org_id=org_id, description=description)
+    new_opportunity = Request(title=title, org_id=org_id, description=description)
 
-    db.session.add(new_request)
+    db.session.add(new_opportunity)
     db.session.commit()
 
     flash('Volunteer request submitted successfully!', 'success')
